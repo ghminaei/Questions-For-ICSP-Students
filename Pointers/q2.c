85838a3bc2053cd756b5b9d0e0b2255411a0f9f3
Ghazal <ghminaei1037@gmail.com>
2019-05-21 21:50:40 +0430

    revert changes

--- Pointers/q2.c
+++ Pointers/q2.c
@@ -6,7 +6,7 @@ int main()
 	const int a = 5;
 	const int *ptr;
 	ptr = &a;
-	*ptr = 10;
+	//*ptr = 10;
 	printf("%d\n", a);
 	return 0;
 }
--- Pointers/q3.c
+++ Pointers/q3.c
@@ -5,7 +5,7 @@ int main()
 {
 	int a = 5;
     const int *ptr = &a;
-    *ptr = 6;
+    //*ptr = 6;
     a = 6;
 	printf("%d\n", a);
 	return 0;
